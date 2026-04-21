805da8c1516fb37a3e859bcc35272e5bbd8c94d7
leo <lehotf@yahoo.com.br>
2026-04-21 07:52:19 -0300

    Correção do cookie.php

--- shared/comum/php/path/lb9/php/autenticacao/cookie.php
+++ shared/comum/php/path/lb9/php/autenticacao/cookie.php
@@ -5,13 +5,6 @@ $c = new controlador(observador: true, autenticador: true);
 
 $c->autenticador->acesso(2);
 
-$autorizacao = isset($_SESSION['autorizacao']) ? (int) $_SESSION['autorizacao'] : 0;
-$c->observador->query(
-    "SELECT m, eval FROM script WHERE (autorizacao <= ?) ORDER BY ordem, m",
-    false,
-    'i',
-    [$autorizacao]
-);
 $c->observador->envia('Autenticado', 'ok');
 
-$c->observador->erro('Acesso Negado');
+
