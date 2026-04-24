1f4c56ee2577d2dd085c3a2f7f40f8bb718d13fa
leo <lehotf@yahoo.com.br>
2026-04-24 18:26:57 -0300

    Simplificação do login

--- shared/comum/php/path/lb9/php/autenticacao/login.php
+++ shared/comum/php/path/lb9/php/autenticacao/login.php
@@ -3,37 +3,11 @@ require $_SERVER['DOCUMENT_ROOT'] . '/comum/php/autoload.php';
 
 $c = new controlador(guardiao: true, logger: true, autenticador: true, observador: true);
 
-function lockout($ip, $logger)
-{    
-    $lockout_file = $_SERVER['DOCUMENT_ROOT'] . '/log/login/' . $ip;
-    $tempo_limite = 15;
-    $tempo_restante = 0;
+$login = $c->observador->texto("login");
+$senha = $c->observador->texto("senha");
 
-    if (file_exists($lockout_file)) {
-        $tempo = time() - filemtime($lockout_file);
-        touch($lockout_file);
-        if ($tempo < $tempo_limite) {
-            $tempo_restante = $tempo_limite - $tempo;
-            $logger->acesso('Tentativa de login bloqueada');
-            return $tempo_restante;
-        }
-    } else {
-        touch($lockout_file);
-    }
-
-    return 0;
-}
-
-if ($tempo_restante = lockout($c->guardiao->getIp(), $c->logger)) {
-    $c->observador->erro("Acesso Negado ({$tempo_restante})");
+if ($c->autenticador->login($login, $senha)) {
+    $c->observador->envia("Autenticado");
 } else {
-    $login = $c->observador->texto("login");
-    $senha = $c->observador->texto("senha");
-
-    if ($c->autenticador->login($login, $senha)) {
-        $c->observador->envia("Autenticado");
-    } else {
-        $c->guardiao->adicionarListaNegra();
-        $c->observador->envia("Acesso Negado");
-    }
+    $c->observador->envia("Acesso Negado");
 }
